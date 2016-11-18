af6180627e68b7be1c27e2c26a0f643117d2d821
Krishnan <krishnoff@gmail.com>
2016-11-18 21:07:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/ui/resume.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'srt.png'));
 });*/
 app.get('/ui/ttab.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'ttab.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'krishnan.jpg'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
