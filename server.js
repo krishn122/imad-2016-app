dd97fa13e79bf46a0e97ac4f1019c7a467684692
Krishnan <krishnoff@gmail.com>
2016-11-26 20:23:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,13 +33,18 @@ app.get('/ui/resume.css', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/show.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'show.css'));
+});
 /*app.get('/ui/srt.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'srt.png'));
 });*/
 app.get('/ui/krishnan.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'krishnan.jpg'));
 });
-
+app.get('/ui/search.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'search.jpg'));
+});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
